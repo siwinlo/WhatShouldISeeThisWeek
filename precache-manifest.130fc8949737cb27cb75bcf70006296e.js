518ed036132306cd5011c05e38efa150d7f1fa4f
Siwin Lo <siwin.lo@gmail.com>
2019-07-24 11:29:30 -0400

    Updates

---
+++ precache-manifest.130fc8949737cb27cb75bcf70006296e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0a49e58b17545de71fa8bb6a812d68",
+    "url": "/WhatShouldISeeThisWeek/index.html"
+  },
+  {
+    "revision": "f2095413e1b72e1ab843",
+    "url": "/WhatShouldISeeThisWeek/static/css/main.fce19dd2.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/WhatShouldISeeThisWeek/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "f2095413e1b72e1ab843",
+    "url": "/WhatShouldISeeThisWeek/static/js/main.5ad49018.chunk.js"
+  },
+  {
+    "revision": "f7893d99a350f66ea1a4",
+    "url": "/WhatShouldISeeThisWeek/static/js/runtime~main.072c2517.js"
+  }
+]);
